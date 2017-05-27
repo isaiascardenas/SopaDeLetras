c50086d31361515ff87acd7a8c71ea71f45c127a
isaiascardenas <isaias.cardenas@usach.cl>
2017-05-27 14:36:41 -0400

    its work

--- Entrega2.java
+++ Entrega2.java
@@ -3,19 +3,6 @@ import java.util.*;
 class Entrega2 {
 
 	public static void main(String[] args) {
-		// List<Integer> coordenadas = new ArrayList<Integer>();
-		// Set<List<Integer>> conjunto = new HashSet<List<Integer>>();
-
-		// coordenadas.add(3);
-		// coordenadas.add(5);
-
-		// conjunto.add(coordenadas);
-
-		// coordenadas.remove(0);
-		// coordenadas.add(0, 1);
-		// System.out.println(coordenadas);
-		// System.out.println(conjunto.contains(coordenadas)); 
-
 
 		FileManager file = new FileManager();
 		LetterSoup soup = new LetterSoup();
@@ -31,17 +18,9 @@ class Entrega2 {
 
 		soup.searchWords();
 
-		System.out.println("\nfinding: "+soup.getFindingWords());
-		// for (String w: soup.getFindingWords()) {
-		// 	System.out.println(w);
-		// }
+		file.setFileName("Solucion.out");
+		file.writeFile(soup.getFindingWords(), soup.getMissingWords());
 
-		System.out.println("\nmissing: "+ soup.getMissingWords());
-		// for (String w: soup.getMissingWords()) {
-		// 	System.out.println(w);
-		// }
-
-		// file.setFileName("Solucion.out");
-		// file.writeFile(soup.getFindingWords(), soup.getMissingWords());
+		System.out.println("Done!");
 	}
 }
--- LetterSoup.java
+++ LetterSoup.java
@@ -19,8 +19,8 @@ public class LetterSoup
 
 	public void setSearchingWords(List<String> searchWords)
 	{
-		this.searchingWords = searchWords;
 		this.missingWords = searchWords;
+		this.searchingWords = searchWords;
 	}
 
 	public void setSoup(List<String> soup)
@@ -35,38 +35,33 @@ public class LetterSoup
 
 	public List<String> getMissingWords()
 	{
+		this.missingWords.removeAll(this.findingWords);
 		return this.missingWords;
 	}
 
 	public void searchWords()
 	{
 		for (String word: this.searchingWords) {
-			System.out.print(word+": ");
 			this.findWord(word);
 		}
 	}
 
 	public void findWord(String word)
 	{
-		// System.out.println("missingWords: "+this.missingWords);
-		// System.out.println("findingWords: "+this.findingWords);
 		for (int j =0 ; j<this.soup.size(); j++) {
 			for (int i = 0; i < this.soup.get(j).length(); i ++) {
 				if (Objects.equals(this.soup.get(j).charAt(i), word.charAt(0))) {
-					List<String> lastPositions = new ArrayList<String>();
+					List<String> lastPositions = new ArrayList<String>(Arrays.asList("notFound"));
 					List<String> backFrom = new ArrayList<String>();
 					List<List<Integer>> set = new ArrayList<List<Integer>>();
 
 					this.coordinates.set(0, i);
 					this.coordinates.set(1, j);
-					System.out.println(this.coordinates);
 					set.add(Arrays.asList(i,j));
 					this.currentChar = 1;
 					if (this.breadthFirstSearch(word, lastPositions, backFrom, set)) {
-						System.out.println("!!!find!!!");
 						this.findingWords.add(0, word);
 						break;
-						// this.missingWords.remove(word);
 					}			
 				}
 			}
@@ -77,20 +72,10 @@ public class LetterSoup
 	{
 		List<Integer> position = new ArrayList<Integer>(Arrays.asList(0,0));
 
-		// System.out.println("Set: "+ positionsSet);
-
-		lastPositions.add(0, "back");
 		lastPositions.add(0, "down");
 		lastPositions.add(0, "up");
 		lastPositions.add(0, "right");
 		lastPositions.add(0, "left");
-
-		if (this.currentChar == 0) {
-			
-			System.out.println("not found 404");
-			return false;
-			//add to missing
-		}
 
 		while (this.currentChar < word.length() && lastPositions.size() != 0) {
 			String direction = lastPositions.get(0);
@@ -101,87 +86,65 @@ public class LetterSoup
 					case "left":
 						position.set(1, this.coordinates.get(1));
 						position.set(0, this.coordinates.get(0)-1);
-						System.out.println("contains "+position+"? "+ positionsSet.contains(position));
 						if (this.soup.get(this.coordinates.get(1)).charAt(this.coordinates.get(0)-1) == word.charAt(this.currentChar) && !positionsSet.contains(position)) {
 							this.coordinates.set(0, this.coordinates.get(0)-1);
-							System.out.println("left: "+this.coordinates);
-							
 							positionsSet.add(0, position);
-							System.out.println("Set: "+positionsSet);
-														
 							this.currentChar = this.currentChar+1;
 							backFrom.add(0, direction);
+							lastPositions.add(0, "back");
 							return this.breadthFirstSearch(word, lastPositions, backFrom, positionsSet);
 						}
 						break;
 					case "right":
 						position.set(1, this.coordinates.get(1));
 						position.set(0, this.coordinates.get(0)+1);
-						System.out.println("contains "+position+"? "+ positionsSet.contains(position));
 						if (this.soup.get(this.coordinates.get(1)).charAt(this.coordinates.get(0)+1) == word.charAt(this.currentChar) && !positionsSet.contains(position)) {
 							this.coordinates.set(0, this.coordinates.get(0)+1);
-							System.out.println("right: "+this.coordinates);
 							positionsSet.add(0, position);
-							System.out.println("Set: "+positionsSet);
-
 							this.currentChar = this.currentChar+1;
 							backFrom.add(0, direction);
+							lastPositions.add(0, "back");
 							return this.breadthFirstSearch(word, lastPositions, backFrom, positionsSet);
 						}
 						break;
 					case "up":
 						position.set(0, this.coordinates.get(0));
 						position.set(1, this.coordinates.get(1)-1);
-						System.out.println("contains "+position+"? "+ positionsSet.contains(position));
 						if (this.soup.get(this.coordinates.get(1)-1).charAt(this.coordinates.get(0)) == word.charAt(this.currentChar) && !positionsSet.contains(position)) {
 							this.coordinates.set(1, this.coordinates.get(1)-1);
-							System.out.println("up: "+this.coordinates);
 							positionsSet.add(0, position);
-							System.out.println("Set: "+positionsSet);
-														
 							this.currentChar = this.currentChar+1;
 							backFrom.add(0, direction);
+							lastPositions.add(0, "back");
 							return this.breadthFirstSearch(word, lastPositions, backFrom, positionsSet);
 						}
 						break;
 					case "down":
 						position.set(0, this.coordinates.get(0));
 						position.set(1, this.coordinates.get(1)+1);
-						System.out.println("contains "+position+"? "+ positionsSet.contains(position));
 						if (this.soup.get(this.coordinates.get(1)+1).charAt(this.coordinates.get(0)) == word.charAt(this.currentChar) && !positionsSet.contains(position)) {
 							this.coordinates.set(1, this.coordinates.get(1)+1);
-							System.out.println("down: "+this.coordinates);
 							positionsSet.add(0, position);
-							System.out.println("Set: "+positionsSet);
-
 							this.currentChar = this.currentChar+1;
 							backFrom.add(0, direction);
+							lastPositions.add(0, "back");
 							return this.breadthFirstSearch(word, lastPositions, backFrom, positionsSet);
 						}
 						break;
-					default:
-						// if (!backFrom.isEmpty()) {
-						// positionsSet.remove(this.coordinates);
+					case "back":
 						position.set(0, this.coordinates.get(0));
 						position.set(1, this.coordinates.get(1));
 						positionsSet.remove(position);
-						// System.out.println("backFrom: "+backFrom);
 						this.goBack(backFrom.get(0));
 						this.currentChar = this.currentChar-1;
 						backFrom.remove(0);
-						// System.out.println("Back: "+this.coordinates);
-						// System.out.println("Set: "+positionsSet);
-						// }
-						// this.coordinates.set(1, y);
-						// this.coordinates.set(0, x);
-						
-						// this.currentChar = this.currentChar-1;
 						break;
+					default:
+						return false;
 				}
 			}
 			
 		}
-		System.out.println("Set: "+positionsSet);
 		return true;
 	}
 
